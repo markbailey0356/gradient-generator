aa8c5992b944d3d2a15feaeadfa48e8ea5d310de
Mark Bailey <markbailey0356@gmail.com>
2021-12-20 11:58:32 +1030

    Generate a gradient from the circle grid

--- src/CircleGrid/CircleGrid.vue
+++ src/CircleGrid/CircleGrid.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
-canvas(ref="canvas" width="1280" height="720")
+canvas(ref="canvas" width="64" height="64" style="width: 1024px; height: 1024px;")
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref, unref } from 'vue';
-import { useRafFn } from '@vueuse/core';
+import { computed, defineComponent, onMounted, ref, unref } from 'vue';
+import { useMouseInElement, useRafFn } from '@vueuse/core';
 
 import instancedCircleVert from './instanced-circle.vert';
 import circleFrag from './circle.frag';
@@ -16,14 +16,23 @@ import { clamp } from 'three/src/math/MathUtils';
 
 export default defineComponent({
 	setup() {
-
 		const canvas = ref(null);
+
+		const mouseInElement = useMouseInElement(canvas);
+		const mouse = computed(() => {
+			const { elementX, elementY, elementHeight, elementWidth } = mouseInElement;
+			return {
+				x: clamp((elementX.value / elementWidth.value) * 2 - 1, -1, 1),
+				y: -clamp((elementY.value / elementHeight.value) * 2 - 1, -1, 1),
+			}
+		})
+
 		onMounted(() => {
 			const renderer = new THREE.WebGLRenderer({ canvas: unref(canvas) });
 
 			const scene = new THREE.Scene();
 
-			const aspectRatio = 16 / 9;
+			const aspectRatio = 1 / 1;
 			const padding = 0;
 			const camera = new THREE.OrthographicCamera();
 			camera.left = -1.0 - padding;
@@ -53,7 +62,7 @@ export default defineComponent({
 				// depthWrite: false,
 			})
 
-			const size = 64;
+			const size = 128;
 
 			const rowCount = size;
 			const columnCount = size;
@@ -74,7 +83,7 @@ export default defineComponent({
 					offsetArray[offsetIndex + 1] = (columnIndex + 0.5) / columnCount * 2 - 1; // y
 					offsetArray[offsetIndex + 2] = -index * 0.01; // z
 
-					scaleArray[index] = 8 / size;
+					scaleArray[index] = 3 / size;
 
 					colorArray[colorIndex + 0] = Math.random();
 					colorArray[colorIndex + 1] = Math.random();
@@ -89,6 +98,19 @@ export default defineComponent({
 			const circles = new THREE.Mesh(geometry, material);
 			scene.add(circles);
 
+			const samples = [
+				new THREE.Vector3(-0.5, -0.5, 0),
+				new THREE.Vector3(0.5, -0.5, 0),
+				new THREE.Vector3(-0.5, 0.5, 0),
+				new THREE.Vector3(0.5, 0.5, 0),
+			]
+
+			const sampleColors = [
+				new THREE.Color(0xff0000),
+				new THREE.Color(0x00ff00),
+				new THREE.Color(0x0000ff),
+				new THREE.Color(0xffff00),
+			]
 
 			const renderTarget = new THREE.WebGLRenderTarget(canvas.value.width, canvas.value.height);
 
@@ -108,6 +130,42 @@ export default defineComponent({
 			postScene.add(postMesh);
 
 			useRafFn(() => {
+				const worldMouse = new THREE.Vector3(mouse.value.x, mouse.value.y, 0).applyMatrix4(camera.projectionMatrixInverse)
+
+				for (let rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+					for (let columnIndex = 0; columnIndex < columnCount; columnIndex++) {
+						const index = (rowIndex * columnCount + columnIndex);
+						const offsetIndex = 3 * index;
+						const colorIndex = 3 * index;
+
+						const offsetX = offsetArray[offsetIndex + 0];
+						const offsetY = offsetArray[offsetIndex + 1];
+
+						let minDistSq = Infinity;
+						let minSampleIndex = 0;
+
+						samples.forEach((sample, index) => {
+							const distSq = (offsetX - sample.x) ** 2 + (offsetY - sample.y) ** 2;
+							if (distSq < minDistSq) {
+								minDistSq = Math.min(minDistSq, distSq);
+								minSampleIndex = index;
+							}
+						});
+
+
+						scaleArray[index] = Math.min(2 * Math.sqrt(minDistSq), 4);
+
+						const color = sampleColors[minSampleIndex];
+
+						colorArray[colorIndex + 0] = color.r;
+						colorArray[colorIndex + 1] = color.g;
+						colorArray[colorIndex + 2] = color.b;
+					}
+				}
+
+				geometry.getAttribute('scale').needsUpdate = true;
+
+
 				renderer.setRenderTarget(renderTarget);
 				renderer.render(scene, camera);
 
